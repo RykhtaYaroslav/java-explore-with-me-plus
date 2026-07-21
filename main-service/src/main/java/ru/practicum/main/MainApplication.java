ac483e797c4c02609b872e2ebc81c78b0178c3b9
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-07-22 03:11:48 +0700

    feat: Add StatsClient workability test

--- main-service/src/main/java/ru/practicum/main/MainApplication.java
+++ main-service/src/main/java/ru/practicum/main/MainApplication.java
@@ -2,11 +2,39 @@ package ru.practicum.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import ru.practicum.stats.client.StatsClient;
+import ru.practicum.stats.dto.EndpointHitDto;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"ru.practicum.main", "ru.practicum.stats.client"})
 public class MainApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+        // Запускем приложение и сохраняем контекст в переменную
+        ConfigurableApplicationContext context = SpringApplication.run(MainApplication.class, args);
+
+        StatsClient statsClient = context.getBean(StatsClient.class);
+
+        EndpointHitDto dto = EndpointHitDto.builder()
+                .app("ewm-main-service")
+                .uri("/test/events/1")
+                .ip("192.168.0.1")
+                .timestamp(LocalDateTime.now()).build();
+
+        // Вызываем метод сохранения статистик
+        statsClient.hit(dto);
+
+        // Вызываем метод получения статистики
+        Object stats = statsClient.getStats(
+                LocalDateTime.now().minusDays(1),
+                LocalDateTime.now().plusDays(1),
+                List.of("/test/events/1"),
+                true
+        );
 
     }
 }
