a3cf3a2348d87b0a6c3f7d32d141bbfb40f3b8ea
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-07-21 16:32:47 +0700

    feat: Add EndpointHit entity and mapper (stat_svc)
    Introduce the `EndpointHit` entity and its corresponding mapper.
    
    This commit includes:
    - Creation of the `EndpointHit` JPA entity to represent an
      endpoint hit in the database.
    - Implementation of the `EndpointHitMapper` interface using
      MapStruct for converting between `EndpointHit` entities and
      `EndpointHitDto`.
    - Addition of necessary Lombok and JPA annotations.

---
+++ stats/stats-server/src/main/java/ru/practicum/stats/model/EndpointHit.java
@@ -0,0 +1,67 @@
+package ru.practicum.stats.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "endpoint_hit")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EndpointHit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "app", nullable = false)
+    private String app;
+
+    @Column(name = "uri", nullable = false)
+    private String uri;
+
+    @Column(name = "ip", nullable = false, length = 45)
+    private String ip;
+
+    @Column(name = "created", nullable = false)
+    private LocalDateTime timestamp;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        Class<?> objectEffectiveClass = o instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != objectEffectiveClass) {
+            return false;
+        }
+        EndpointHit that = (EndpointHit) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
