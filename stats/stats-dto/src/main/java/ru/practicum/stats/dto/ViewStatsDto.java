6556d3b410fedda9ebee1ada3a0d469a2f30f795
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-07-21 16:30:45 +0700

    feat: Add DTOs for stats service (stat_svc)
    Introduce Data Transfer Objects (DTOs) for the statistics service.
    
    This commit includes:
    - Creation of `ViewStatsDto` for statistics retrieval.
    - Creation of `EndpointHitDto` for recording hits.
    - Addition of necessary Lombok annotations and validation constraints.

---
+++ stats/stats-dto/src/main/java/ru/practicum/stats/dto/ViewStatsDto.java
@@ -0,0 +1,18 @@
+package ru.practicum.stats.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ViewStatsDto {
+    private String app;
+    private String uri;
+    private Long hits;
+}
