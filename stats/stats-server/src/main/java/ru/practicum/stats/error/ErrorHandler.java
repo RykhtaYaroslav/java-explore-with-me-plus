a7c8f18fd2b1e9c56e778f3a5643a3b66fd9c156
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-07-22 20:27:47 +0700

    fix: Add logging to ErrorHandler and improve BadRequest message (stat_svc)
    Adds SLF4j logging to the ErrorHandler for bad request exceptions.
    Also, enhances the error message for the BadRequestException
    in StatController to include the provided start and end dates
    for better debugging.

--- stats/stats-server/src/main/java/ru/practicum/stats/error/ErrorHandler.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/error/ErrorHandler.java
@@ -1,5 +1,6 @@
 package ru.practicum.stats.error;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -9,18 +10,21 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleBadRequest(BadRequestException e) {
-        return ErrorResponse.builder()
+        ErrorResponse errorResponse = ErrorResponse.builder()
                 .status(HttpStatus.BAD_REQUEST.name())
                 .reason("Некорректный запрос")
                 .message(e.getMessage())
                 .timestamp(LocalDateTime.now())
                 .errors(getErrorsList(e))
                 .build();
+        log.error("Получена ошибка 400 Bad Request: {}", e.getMessage(), e);
+        return errorResponse;
     }
 
     private List<String> getErrorsList(Exception e) {
