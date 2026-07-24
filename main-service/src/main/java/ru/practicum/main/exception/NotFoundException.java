bbae59c70376e436066fe59f4bd5b99c9dc81070
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-07-24 17:22:46 +0700

    feat(exception): add NotFoundException
    Introduces a custom `NotFoundException` class.
    This exception is designed to be thrown when a requested
    resource is not found. It includes fields to store the
    name of the field being searched and the rejected value,
    providing more context for debugging and error handling.

---
+++ main-service/src/main/java/ru/practicum/main/exception/NotFoundException.java
@@ -0,0 +1,15 @@
+package ru.practicum.main.exception;
+
+import lombok.Getter;
+
+
+@Getter
+public class NotFoundException extends RuntimeException {
+    private final String fieldName;
+    private final transient Object rejectedValue;
+    public NotFoundException(String message, String fieldName, Object rejectedValue) {
+        super(message);
+        this.fieldName = fieldName;
+        this.rejectedValue = rejectedValue;
+    }
+}
