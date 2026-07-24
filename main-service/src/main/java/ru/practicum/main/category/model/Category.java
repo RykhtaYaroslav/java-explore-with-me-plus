0af42a9d86302418cecec2c0059812288e7a096d
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-07-24 19:12:40 +0700

    feat(model): add User and Category models
    Adds common Lombok annotations (@Getter, @Setter, @Builder,
    @NoArgsConstructor, @AllArgsConstructor) to User and Category
    model classes. This reduces boilerplate code and standardizes
    model definitions.
    (main_svc_events)

--- main-service/src/main/java/ru/practicum/main/category/model/Category.java
+++ main-service/src/main/java/ru/practicum/main/category/model/Category.java
@@ -1,4 +1,17 @@
 package ru.practicum.main.category.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
+    private Long id;
+    private String name;
 }
--- main-service/src/main/java/ru/practicum/main/user/model/User.java
+++ main-service/src/main/java/ru/practicum/main/user/model/User.java
@@ -1,4 +1,18 @@
 package ru.practicum.main.user.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    private Long id;
+    private String name;
+    private String email;
 }
