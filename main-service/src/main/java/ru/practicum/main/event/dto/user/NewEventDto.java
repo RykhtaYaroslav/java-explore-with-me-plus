3a669384b5153b4648d90a2b32e09c2e53e9b800
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-07-24 15:43:00 +0700

    feat(event): add NewEventDto with validation
    Introduces `NewEventDto` for creating new events.
    This DTO includes fields for title, annotation,
    description, category ID, event date, location,
    paid status, participant limit, and request
    moderation.
    
    It incorporates validation annotations for:
    - `NotBlank` and `Size` for string fields.
    - `NotNull` for category ID, event date, and
      location.
    - `FutureAfterHours` for event date to ensure
      it's at least 2 hours in the future.
    - `PositiveOrZero` for participant limit.
    - `@Valid` for nested `LocationDto`.
    - Default values for `paid`, `participantLimit`,
      and `requestModeration`.

--- main-service/src/main/java/ru/practicum/main/event/dto/user/NewEventDto.java
+++ main-service/src/main/java/ru/practicum/main/event/dto/user/NewEventDto.java
@@ -1,4 +1,73 @@
 package ru.practicum.main.event.dto.user;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ru.practicum.main.event.dto.LocationDto;
+import ru.practicum.main.util.validation.FutureAfterHours;
+
+import java.time.LocalDateTime;
+
+import static ru.practicum.main.util.EwmConstants.DATE_TIME_FORMAT;
+import static ru.practicum.main.util.EwmConstants.MAX_ANNOT_LENGTH;
+import static ru.practicum.main.util.EwmConstants.MAX_DESC_LENGTH;
+import static ru.practicum.main.util.EwmConstants.MAX_TITLE_LENGTH;
+import static ru.practicum.main.util.EwmConstants.MIN_ANNOTATION_LENGTH;
+import static ru.practicum.main.util.EwmConstants.MIN_DESCRIPTION_LENGTH;
+import static ru.practicum.main.util.EwmConstants.MIN_TITLE_LENGTH;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class NewEventDto {
+    @NotBlank(message = "Event title must not be blank")
+    @Size(min = MIN_TITLE_LENGTH, max = MAX_TITLE_LENGTH,
+            message = "Title length must be from {min} to {max}")
+    private String title;
+
+    @NotBlank(message = "Event annotation must not be blank")
+    @Size(min = MIN_ANNOTATION_LENGTH, max = MAX_ANNOT_LENGTH,
+            message = "Annotation length must be from {min} to {max}")
+    private String annotation;
+
+    @NotBlank(message = "Event description must not be blank")
+    @Size(
+            min = MIN_DESCRIPTION_LENGTH,
+            max = MAX_DESC_LENGTH,
+            message = "Description length must be from {min} to {max}")
+    private String description;
+
+    @NotNull(message = "Category ID must not be null")
+    @JsonProperty("category")
+    private Long categoryId;
+
+    @NotNull(message = "Event date must not be null")
+    @FutureAfterHours(hours = 2, message = "Event date cannot be earlier than {hours} hours from now")
+    @JsonFormat(pattern = DATE_TIME_FORMAT)
+    private LocalDateTime eventDate;
+
+    @NotNull
+    @Valid
+    private LocationDto location;
+
+    @Builder.Default
+    private Boolean paid = false;
+
+    @PositiveOrZero(message = "Participant limit must be positive or zero")
+    @Builder.Default
+    private Integer participantLimit = 0;
+
+    @Builder.Default
+    private Boolean requestModeration = true;
 }
