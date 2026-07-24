9a4b59f21a3bd49503dc2fdf4d56224fce9aa15c
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-07-24 16:26:03 +0700

    feat(event): add EventFullDto
    Introduces EventFullDto for detailed event representation.
    This DTO includes all fields necessary to fully describe
    an event, such as ID, title, annotation, description,
    category, dates (created, event, published), initiator,
    location, paid status, participant limit, moderation
    status, state, confirmed requests, and views.

--- main-service/src/main/java/ru/practicum/main/event/dto/publicapi/EventFullDto.java
+++ main-service/src/main/java/ru/practicum/main/event/dto/publicapi/EventFullDto.java
@@ -1,4 +1,47 @@
 package ru.practicum.main.event.dto.publicapi;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ru.practicum.main.category.dto.CategoryDto;
+import ru.practicum.main.event.dto.LocationDto;
+import ru.practicum.main.event.model.EventState;
+import ru.practicum.main.user.dto.UserShortDto;
+
+import java.time.LocalDateTime;
+
+import static ru.practicum.main.util.EwmConstants.DATE_TIME_FORMAT;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class EventFullDto {
+    private Long id;
+    private String title;
+    private String annotation;
+    private String description;
+    private CategoryDto category;
+
+    @JsonFormat(pattern = DATE_TIME_FORMAT)
+    private LocalDateTime createdOn;
+
+    @JsonFormat(pattern = DATE_TIME_FORMAT)
+    private LocalDateTime eventDate;
+
+    @JsonFormat(pattern = DATE_TIME_FORMAT)
+    private LocalDateTime publishedOn;
+
+    private UserShortDto initiator;
+    private LocationDto location;
+    private Boolean paid;
+    private Integer participantLimit;
+    private Boolean requestModeration;
+    private EventState state;
+    private Long confirmedRequests;
+    private Long views;
 }
