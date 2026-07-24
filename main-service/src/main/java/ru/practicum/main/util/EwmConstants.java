f2ee4f81e5a19cd065dc0cbf9ba3922dded2f4f8
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-07-24 16:27:13 +0700

    feat(util): make EwmConstants final
    Makes the `EwmConstants` class final.
    This change prevents the class from being extended,
    enforcing its intended use as a utility class with
    static members.

--- main-service/src/main/java/ru/practicum/main/util/EwmConstants.java
+++ main-service/src/main/java/ru/practicum/main/util/EwmConstants.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 
 @UtilityClass
 @SuppressWarnings("unused")
-public class EwmConstants {
+public final class EwmConstants {
     public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT);
