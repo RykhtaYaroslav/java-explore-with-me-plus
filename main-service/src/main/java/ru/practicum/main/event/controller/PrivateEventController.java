bc0439d0d209aa2457125853c3896b6b1dd46666
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-07-26 02:20:00 +0700

    fix(controller): add @Positive to userId path variable
    Applies the `@Positive` annotation to the `userId` path variable in the
    `PrivateEventController.create` method. This ensures that the `userId`
    is a positive number, enhancing data validation and integrity.
    (main_svc_events)

--- main-service/src/main/java/ru/practicum/main/event/controller/PrivateEventController.java
+++ main-service/src/main/java/ru/practicum/main/event/controller/PrivateEventController.java
@@ -1,9 +1,10 @@
 package ru.practicum.main.event.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,12 +18,13 @@ import ru.practicum.main.event.service.EventService;
 @RestController
 @RequestMapping("/users/{userId}/events")
 @RequiredArgsConstructor
+@Validated
 public class PrivateEventController {
     private final EventService eventService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public EventFullDto create(@RequestBody @Valid NewEventDto newEventDto, @PathVariable Long userId) {
+    public EventFullDto create(@RequestBody @Valid NewEventDto newEventDto, @PathVariable @Positive Long userId) {
         return eventService.create(userId, newEventDto);
     }
 }
