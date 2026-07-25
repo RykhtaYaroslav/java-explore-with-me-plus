49c2e69c06fbd77831241929c6b6f8342c84598c
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-07-26 02:12:02 +0700

    feat(controller): add @ResponseStatus to event creation
    Adds `@ResponseStatus(HttpStatus.CREATED)` annotation to the
    `create` method in `PrivateEventController`. This ensures that
    event creation requests correctly return a 201 Created status
    code upon successful creation. (main_svc_events)

--- main-service/src/main/java/ru/practicum/main/event/controller/PrivateEventController.java
+++ main-service/src/main/java/ru/practicum/main/event/controller/PrivateEventController.java
@@ -2,10 +2,13 @@ package ru.practicum.main.event.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.main.event.dto.publicapi.EventFullDto;
 import ru.practicum.main.event.dto.user.NewEventDto;
@@ -18,6 +21,7 @@ public class PrivateEventController {
     private final EventService eventService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public EventFullDto create(@RequestBody @Valid NewEventDto newEventDto, @PathVariable Long userId) {
         return eventService.create(userId, newEventDto);
     }
