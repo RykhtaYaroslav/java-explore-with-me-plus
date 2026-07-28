ad4721ea9e76ed6458cac057ab486866a8cec31d
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-07-28 20:50:40 +0700

    feat(request): add ParticipationRequestDto (main_svc_events)
    Adds the `ParticipationRequestDto` class to represent participation
    requests. This DTO includes fields for the request ID, description,
    creation timestamp, event ID, requester ID, and request status.
    
    This DTO will be used to transfer participation request data between
    the client and the server, and for internal processing within the
    application.

--- main-service/src/main/java/ru/practicum/main/request/dto/ParticipationRequestDto.java
+++ main-service/src/main/java/ru/practicum/main/request/dto/ParticipationRequestDto.java
@@ -1,4 +1,29 @@
 package ru.practicum.main.request.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.main.request.model.RequestStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ParticipationRequestDto {
+
+    private Long id;
+    private String description;
+    private LocalDateTime created;
+
+    @JsonProperty("event")
+    private Long eventId;
+
+    @JsonProperty("requester")
+    private Long requesterId;
+
+    private RequestStatus status;
 }
