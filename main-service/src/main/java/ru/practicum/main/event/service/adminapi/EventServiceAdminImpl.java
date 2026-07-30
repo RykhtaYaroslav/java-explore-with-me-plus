10592eee8691f09bdf790c61a975541aef1f48af
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-07-30 18:08:10 +0700

    refactor(event): introduce Public and Admin event service interfaces
    Introduces distinct interfaces for public and admin event services.
    Creates EventServicePublic and EventServiceAdmin interfaces, along
    with their respective implementation classes, EventServicePublicImpl
    and EventServiceAdminImpl. This change separates concerns and
    clarifies the API layers for event management.
    
    Also, comments out the 'test' profile in application.properties.

---
+++ main-service/src/main/java/ru/practicum/main/event/service/adminapi/EventServiceAdminImpl.java
@@ -0,0 +1,4 @@
+package ru.practicum.main.event.service.adminapi;
+
+public class EventServiceAdminImpl implements EventServiceAdmin {
+}
---
+++ main-service/src/main/java/ru/practicum/main/event/service/publicapi/EventServicePublic.java
@@ -0,0 +1,4 @@
+package ru.practicum.main.event.service.publicapi;
+
+public interface EventServicePublic {
+}
