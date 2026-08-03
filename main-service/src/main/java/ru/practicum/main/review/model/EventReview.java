7b4d06eccf32a457a68e87b844e003f42850cafe
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-08-03 22:45:38 +0700

    feat(review): add EventReview model and EventReviewId
    This commit introduces the `EventReview` model and its composite primary key
    `EventReviewId`. The `EventReview` model represents a review left by a user
    for an event.
    
    The `EventReviewId` class is a `Serializable` entity that combines `raterId`
    and `eventId` to uniquely identify an event review.
    
    The `EventReview` entity includes fields for:
    - `raterId`: The ID of the user who wrote the review.
    - `eventId`: The ID of the event being reviewed.
    - `score`: The rating score given in the review.
    - `comment`: An optional textual comment for the review.
    
    This change lays the groundwork for managing user reviews of events within
    the application.

--- main-service/src/main/java/ru/practicum/main/review/model/EventReview.java
+++ main-service/src/main/java/ru/practicum/main/review/model/EventReview.java
@@ -1,4 +1,65 @@
 package ru.practicum.main.review.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@IdClass(EventReview.class)
+@Table(name = "events_reviews")
+@Entity
 public class EventReview {
+    @Id
+    @Column(name = "rater_id")
+    private Long raterId;
+
+    @Id
+    @Column(name = "event_id")
+    private Long eventId;
+
+    @Column(name = "score", nullable = false)
+    private Integer score;
+
+    @Column(name = "comment", length = 200)
+    private String comment;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        Class<?> objectEffectiveClass = o instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != objectEffectiveClass) {
+            return false;
+        }
+        EventReview that = (EventReview) o;
+        return getRaterId() != null && Objects.equals(getRaterId(), that.getRaterId())
+                && getEventId() != null && Objects.equals(getEventId(), that.getEventId())
+                && getScore() != null && Objects.equals(getScore(), that.getScore())
+                && getComment() != null && Objects.equals(getComment(), that.getComment());
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(raterId,
+                eventId,
+                score,
+                comment);
+    }
 }
---
+++ main-service/src/main/java/ru/practicum/main/review/model/EventReviewId.java
@@ -0,0 +1,17 @@
+package ru.practicum.main.review.model;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class EventReviewId {
+    private Long raterId;
+    private Long eventId;
+}
