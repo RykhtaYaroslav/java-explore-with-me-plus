0f692b8f87381fc82b00dc17ce8d4bd3cc085336
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-08-05 21:12:32 +0700

    feat(review): update DTOs to use UserShortDto and EventShortDto
    This commit updates the `OutputEventReviewDto` and `OutputUserReviewDto`
    classes.
    
    The `raterId` and `eventId` fields in `OutputEventReviewDto` are now
    represented by `UserShortDto` and `UserShortDto` respectively.
    
    The `raterId`, `targetId`, and `eventId` fields in `OutputUserReviewDto`
    are now represented by `UserShortDto`, `UserShortDto`, and
    `EventShortDto` respectively.
    
    This change provides more detailed information about the related entities
    when returning review data.

--- main-service/src/main/java/ru/practicum/main/review/dto/OutputUserReviewDto.java
+++ main-service/src/main/java/ru/practicum/main/review/dto/OutputUserReviewDto.java
@@ -4,15 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.practicum.main.event.dto.EventShortDto;
+import ru.practicum.main.user.dto.UserShortDto;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class OutputUserReviewDto {
-    private Long raterId;
-    private Long targetId;
-    private Long eventId;
+    private UserShortDto raterId;
+    private UserShortDto targetId;
+    private EventShortDto eventId;
     private Integer score;
     private String comment;
 }
